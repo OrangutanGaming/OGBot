b8449639b7d99ae5e865e060d9d5a32bce1232e2
Nihaal Sangha <nihaal.s@live.co.uk>
2017-05-30 16:29:37 +0100

    Redone counts

--- cogs/count.py
+++ cogs/count.py
@@ -14,39 +14,37 @@ class Count():
             user = ctx.message.author
         if not channel:
             channel = ctx.message.channel
-        async for log in channel.history(limit=99, before=ctx.message):
+        async for log in channel.history(before=ctx.message, limit=999):
             if log.author == user:
                 counter += 1
         await ctx.message.delete()
-        counter += 1
-        if counter == 100:
-            await tmp.edit(content="{} has at least {} messages in {}".format(user, counter, channel.mention))
-        elif counter == 1:
-            await tmp.edit(content="{} has 1 message in {}".format(user, channel.mention))
-        elif counter <= 99:
-            await tmp.edit(content="{} has {} messages in {}".format(user, counter, channel.mention))
+        if counter == 1000: # Max
+            await tmp.edit(content=f"{user} has sent {counter} messages in {channel.mention}") # ~over 1000 (cap)
+        elif counter == 1: # 0 before command
+            await tmp.edit(content=f"{user} has sent 1 message in {channel.mention}") # No Plural
+        elif counter <= 999 or counter == 0: # 2-999
+            await tmp.edit(content=f"{user} has sent {counter} messages in {channel.mention}")
         else:
-            await tmp.edit(content="Counter Bug")
+            await tmp.edit(content="Counter Bug") # Shouldn't be possible
     
     @commands.command(aliases=["amcount"], no_pm=True)
     async def amsgcount(self, ctx, channel: discord.TextChannel = None):
         """Counts the amount of messages in the channel given."""
-        counter = 0
         tmp = await ctx.send(self.bot.blank + "Counting messages...")
         if not channel:
             channel = ctx.message.channel
-        async for message in channel.history(before=ctx.message, limit=99):
-            counter += 1
+        history = await channel.history(before=ctx.message, limit=999).flatten()
+        counter = len(history)
         await ctx.message.delete()
-        counter += 1
-        if counter == 100:
-            await tmp.edit(content="There are now at least {} messages in {}".format(counter, channel.mention))
-        elif counter == 1:
-            await tmp.edit(content="There is now 1 message in {}".format(channel.mention))
-        elif counter <= 99:
-            await tmp.edit(content="There are now {} messages in {}".format(counter, channel.mention))
+        counter += 1 # To include tmp. 1<=counter<=1000
+        if counter == 1000: # Max
+            await tmp.edit(content=f"There are now at least {counter} messages in {channel.mention}") # ~over 1000 (cap)
+        elif counter == 1: # 0 before command
+            await tmp.edit(content=f"There is now 1 message in {channel.mention}") # No Plural
+        elif counter <= 999: # 2-999
+            await tmp.edit(content=f"There are now {counter} messages in {channel.mention}")
         else:
-            await tmp.edit(content="Counter Bug")
+            await tmp.edit(content="Counter Bug") # Shouldn't be possible
 
 def setup(bot):
     bot.add_cog(Count(bot))
