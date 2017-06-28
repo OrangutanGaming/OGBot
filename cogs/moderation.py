078b2067cc6d167578ba053c088f6a79255e5c6c
Nihaal Sangha <nihaal.s@live.co.uk>
2017-06-28 19:30:24 +0100

    Fixed typos in moderation

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -9,30 +9,29 @@ class Moderation():
     @commands.command()
     @checks.has_permissions_owner(kick_members=True)
     async def kick(self, ctx, member: discord.Member = None, *, reason: str = None):
-        async def ban(self, ctx, member: discord.Member = None, *, reason: str = None):
-            if not ctx.message.channel.permissions_for(ctx.guild.me).kick_members:
-                await ctx.send("I need the permission `Ban Members`.")
-                return
+        if not ctx.message.channel.permissions_for(ctx.guild.me).kick_members:
+            await ctx.send("I need the permission `Kick Members`.")
+            return
 
-            if not member:
-                await ctx.send("You must give a user!")
-                return
+        if not member:
+            await ctx.send("You must give a user!")
+            return
 
-            if member == ctx.guild.owner:
-                await ctx.send("You can't kick the owner.")
-                return
+        if member == ctx.guild.owner:
+            await ctx.send("You can't kick the owner.")
+            return
 
-            try:
-                await member.kick(reason=reason)
-            except discord.Forbidden:
-                await ctx.send("Error")
-                return
+        try:
+            await member.kick(reason=reason)
+        except discord.Forbidden:
+            await ctx.send("Error")
+            return
 
     @commands.command()
     @checks.has_permissions_owner(ban_members=True)
     async def ban(self, ctx, member: discord.Member = None, *, reason: str = None):
         if not ctx.message.channel.permissions_for(ctx.guild.me).ban_members:
-            await ctx.send("I need the permission `Kick Members`.")
+            await ctx.send("I need the permission `Ban Members`.")
             return
 
 
